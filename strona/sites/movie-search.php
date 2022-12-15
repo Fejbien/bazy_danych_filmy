7de0fe6f09a6ab9554e183196d4adfa050bd7b6d
fejbien <sucholasfejbien@gmail.com>
2022-12-15 11:46:52 +0100

    dodane inne wyszukanie bo nie bylo tego o co chodzilo

--- strona/sites/movie-search.php
+++ strona/sites/movie-search.php
@@ -14,6 +14,7 @@
             <?php include $_SERVER['DOCUMENT_ROOT'].'/strona/includes/nav.php'; ?>
             <div class="aside">
                 <h1>Dostępne filmy</h1>
+                <div style="height: 200px; overflow-y: scroll; display:flex; flex-wrap: wrap; flex-direction: row; justify-content: center; align-content: flex-start; border-bottom: 1px solid white">
                 <?php
                     $con = new mysqli("localhost", "root", "", "movies");
                     $sql = "SELECT `id`, `name`, `year` FROM `movies` WHERE `renter_id` is null and `admin_id` is not null;";
@@ -24,6 +25,25 @@
                             <p>".$row["name"]."</p>
                             <p>".$row["year"]."r.</p>
                         </a>";
+                    }
+                ?>
+                </div>
+                <h1>Lub szukaj poprzez tytuł:</h1>
+                <form method="POST">
+                    <input type="text" name="title">
+                    <input type="submit" value="Znajdz!">
+                </form>
+                <?php
+                    if(isset($_POST["title"]) && !empty($_POST["title"])){
+                        $sql = "SELECT `id`, `name`, `year` FROM `movies` WHERE `name` LIKE '%".$_POST["title"]."%' && `renter_id` is null and `admin_id` is not null;";
+                        $res = $con->query($sql);
+                        while($row = $res->fetch_assoc()){
+                            echo "
+                            <a class='card' href='/strona/sites/movie-details.php?movieId=".$row["id"]."'>
+                                <p>".$row["name"]."</p>
+                                <p>".$row["year"]."r.</p>
+                            </a>";
+                        }
                     }
                 ?>
             </div>
